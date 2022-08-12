6304f3f2469f83898b8355ae7aa3e8df74850258
zionhigt <larcher501@gmail.com>
2022-08-12 02:06:24 +0200

    INIT Public

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const app = express();
 
-exports.module = app;
+const path = require("path");
+
+app.use(["/"], express.static(path.join(__dirname, "public")));
+app.use(function(req, res, next) {
+    if (req.headers.referer) {
+        console.log([
+            "\x1b[35m" + new Date(),
+            "\x1b[36m" + req.method,
+            "\x1b[33m" + req.headers.referer
+        ].join("  ")  + "\x1b[37m");
+    }
+    next();
+})
+module.exports = app;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const app = require("./app.js");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "localhost";
 const server = http.createServer(app);
 
 server.on("error", function(err) {
@@ -10,9 +11,8 @@ server.on("error", function(err) {
 server.on("listening", function() {
     console.log("server is listening on")
     console.table({
-        host: process.env.HOST,
+        host: HOST,
         port: PORT
     })
 })
-console.log(server)
 server.listen(PORT)
